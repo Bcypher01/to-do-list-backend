a341fb574bdde4ae904f03dd7f8bb864254e080d
Bcypher01 <olumidebabayemi1@gmail.com>
2022-10-01 07:24:26 +0100

    Add todo model

--- todo_api/models.py
+++ todo_api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Status(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+class ToDo(models.Model):
+    name = models.CharField(max_length=200)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, null=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-updated', 'created']
+    
+    def __str__(self):
+        return self.name
+        
