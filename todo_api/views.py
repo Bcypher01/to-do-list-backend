08ddfb20af9f100ce38641ba84421cb30e2c5b7f
Bcypher01 <olumidebabayemi1@gmail.com>
2022-10-11 11:02:08 +0100

    add post view

--- todo_api/views.py
+++ todo_api/views.py
@@ -11,4 +11,11 @@ class ToDoApiView(APIView):
         toDo = ToDo.objects.all()
         serializer = ToDoSerializer(toDo, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ToDoSerializer(data=request.data)
         
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
