f9844db5e55cefbfcf6239ac5062c681e54e4b30
Bcypher01 <olumidebabayemi1@gmail.com>
2022-10-11 08:02:50 +0100

    alter field progress in todo model

--- todo_api/models.py
+++ todo_api/models.py
@@ -10,7 +10,7 @@ class Status(models.Model):
 class ToDo(models.Model):
     name = models.CharField(max_length=200)
     status = models.ForeignKey(Status, on_delete=models.CASCADE, null=True)
-    progress = models.PositiveSmallIntegerField(default=1, blank=True, null=True)
+    progress = models.PositiveSmallIntegerField(default=0, blank=True, null=True)
     due_date = models.DateTimeField(null=True)
     created = models.DateTimeField(auto_now_add=True)
     
