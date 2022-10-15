c01dad6ae7eccf7d5f3d37e3bcf1c20d51ce8441
Bcypher01 <olumidebabayemi1@gmail.com>
2022-10-15 19:22:23 +0100

    Add edit todo view

--- todo_api/views.py
+++ todo_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from todo_api.models import Status, ToDo
 from todo_api.serializers import StatusSerializer, ToDoSerializer
+from rest_framework import status
 
 # Create your views here.
 class StatusApiView(APIView):
@@ -24,3 +25,11 @@ class ToDoApiView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class TodoDetail(APIView):
+    def get_object(self, id):
+        try:
+            return ToDo.objects.get(id=id)
+        
+        except ToDo.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
